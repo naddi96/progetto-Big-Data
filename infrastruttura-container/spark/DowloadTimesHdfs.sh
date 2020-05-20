2895d4a0da2027dd253a5a213d303860138bd675
naddi <damiano@ippoagri.it>
2020-05-20 14:22:48 +0200

    aggiunto script per scaricare tempi

---
+++ infrastruttura-container/spark/DowloadTimesHdfs.sh
@@ -0,0 +1,4 @@
+ docker exec mycluster-master hadoop fs -fs  hdfs://mycluster-master:9000 -copyToLocal /time_query1/ /myapp/
+ docker exec mycluster-master hadoop fs -fs  hdfs://mycluster-master:9000 -copyToLocal /time_query2/ /myapp/
+
+
