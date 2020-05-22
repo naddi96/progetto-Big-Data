a89df0526ece1bb017340c08c68eb3ca128cc504
naddi <damiano@ippoagri.it>
2020-05-22 15:08:39 +0200

    minor changes

--- infrastruttura-container/hbase/startHbase.sh
+++ infrastruttura-container/hbase/startHbase.sh
@@ -1,5 +1,5 @@
 docker run -d --network=mycluster -p 16000:16000 -p 2181:2181 -p 16010:16010 --name=hbase-docker -h hbase-docker harisekhon/hbase:1.4
 sleep 30
-sudo docker exec hbase-docker /bin/bash -c "echo \"create 'covidIta', '19Ita'\" | /hbase/bin/hbase shell -n"
-sudo docker exec hbase-docker /bin/bash -c "echo \"create 'covidWor', '19Wor'\" | /hbase/bin/hbase shell -n"
+docker exec hbase-docker /bin/bash -c "echo \"create 'covidIta', '19Ita'\" | /hbase/bin/hbase shell -n"
+docker exec hbase-docker /bin/bash -c "echo \"create 'covidWor', '19Wor'\" | /hbase/bin/hbase shell -n"
 
--- infrastruttura-container/nifi/startNifi.sh
+++ infrastruttura-container/nifi/startNifi.sh
@@ -2,3 +2,10 @@ docker run --name nifi --network=mycluster -p 9090:9090 -d -e NIFI_WEB_HTTP_PORT
 docker exec nifi mkdir /home/nifi/hdfs_conf
 docker cp ../spark/config/core-site.xml nifi:/home/nifi/hdfs_conf/core-site.xml
 docker cp ../spark/config/core-site.xml nifi:/home/nifi/hdfs_conf/hdfs-site.xml
+docker exec -u 0 nifi  chown nifi /home/nifi/hdfs_conf/core-site.xml
+docker exec -u 0 nifi  chown nifi /home/nifi/hdfs_conf/hdfs-site.xml
+
+
+
+
+
